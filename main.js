08036406a0940318db061c1010b7113fb11576d2
Komal Meena <capkomal2203@gmail.com>
2025-04-28 20:56:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".text", {
-  strings: ["Fullstack Developer 🚀", "Coffee Enthusiast ☕", "UI/UX Explorer 🎨"],
-  typeSpeed: 60,
-  backSpeed: 60,
-  backDelay: 30,
+  strings: ["Fullstack Developer", "UI/UX Enthusiast","Coffee Lover"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 40,
   loop: true,
 });
